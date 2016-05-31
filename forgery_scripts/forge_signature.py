2dc28ad7b6382afa7d13dfb04905647af2ec0d87
Xavier Chapron <chapron.xavier@gmail.com>
2016-05-31 16:28:44 +0200

    Adding a python script actually able to get the contain of to files and to store it in strings

---
+++ forgery_scripts/forge_signature.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+'''The aim of this file is to create a script that can be call with 2 arguments :
+- The message to signed
+- The signature to copy
+
+Can be used with :
+./forge_signature.py ../inputs/message_a_signer.txt ../certifs/CA/signature
+'''
+
+
+from optparse import OptionParser
+import sys
+
+parser = OptionParser()
+parser.usage = """%prog messageFile signatureFile\nFor exemple: \n./forge_signature.py ../inputs/message_a_signer.txt ../certifs/CA/signature"""
+parser.description = "Create a signature for a specified file"
+
+(options, args) = parser.parse_args()
+
+if len(args) != 2:
+    parser.print_help(file=sys.stderr)
+    sys.exit(1)
+
+fd = open(args[0])
+message = fd.read()
+fd.close()
+
+fd = open(args[1])
+signature = fd.read()
+fd.close()
+
+print "message: %s/n" % message
+print "signature : %s/n" % signature
